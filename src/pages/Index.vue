bca99b1b96a48186b92d7c7bf0ed000a7fd923ab
Rolland Orgonas <rorgonas@gmail.com>
2021-02-20 09:04:33 -0500

    wheater-app - add input text field

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,36 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+  <q-page class="flex column">
+    <div class="col q-pt-lg q-px-md">
+      <q-input
+        v-model="search"
+        placeholder="Search"
+        dark
+        borderless
+      >
+        <template v-slot:before>
+          <q-icon name="my_location" />
+        </template>
+
+        <template v-slot:append>
+          <q-btn round dense flat icon="search" />
+        </template>
+      </q-input>
+    </div>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data() {
+    return {
+      search: ''
+    }
+  }
 }
 </script>
+
+<style lang="sass">
+  .q-page
+    background: linear-gradient(to top, #B06AB3, #4568DC)
+</style>
