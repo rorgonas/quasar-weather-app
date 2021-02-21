369f6b9d2b777be00751e2023d6a06fa56cd35a5
Rolland Orgonas <rorgonas@gmail.com>
2021-02-21 09:17:24 -0500

    wheater-app - update - electron app window size

--- src-electron/main-process/electron-main.js
+++ src-electron/main-process/electron-main.js
@@ -21,8 +21,8 @@ function createWindow () {
    * Initial window options
    */
   mainWindow = new BrowserWindow({
-    width: 1000,
-    height: 600,
+    width: 450,
+    height: 700,
     useContentSize: true,
     webPreferences: {
       // Change from /quasar.conf.js > electron > nodeIntegration;
